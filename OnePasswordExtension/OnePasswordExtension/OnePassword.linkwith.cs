b9d5215e1ebbb8935344c38a4ffb4a5dfc3f48cb
Ian Levesque <ian@ianlevesque.org>
2015-03-15 01:27:34 -0400

    More backward compatible.

--- OnePasswordExtension/OnePasswordExtension/OnePassword.linkwith.cs
+++ OnePasswordExtension/OnePasswordExtension/OnePassword.linkwith.cs
@@ -4,6 +4,4 @@ using ObjCRuntime;
 [assembly: LinkWith (
     "OnePassword.a", 
     LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.ArmV7 | LinkTarget.Arm64, 
-    SmartLink = true, 
-    ForceLoad = true,
-    Frameworks = "MobileCoreServices WebKit")]
+    WeakFrameworks = "MobileCoreServices WebKit")]
